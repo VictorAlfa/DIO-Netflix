3285afc798774c52ff5d13d98ff64dfdbb329f45
VictorAlfa <victorosantos@yahoo.com.br>
2022-07-21 23:52:39 -0300

    Conclusão do projeto de recriação da interface do Netflix

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function link_assistir(){
+    window.open("https://www.youtube.com/watch?v=9GgxinPwAGc");
+}
+function link_informacao(){
+    window.open("https://www.themoviedb.org/tv/44217-vikings");
+}
